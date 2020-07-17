210a85ccbdb73196c45e39d5d0d566bfc5ae2626
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-07-17 16:30:39 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="kk">
+    <h1>Hello!! I am the parent....</h1>
+    <!-- <button @click="passit">Pass name</button> -->
+    <input type="text" v-model="childname">
+    <Child :childname="childname"></Child>
+  </div>
+</template>
+
+<script>
+import child from './child';
+export default {
+  components:{
+    Child : child
+  },
+  data : function (){
+    return{
+      childname : "kartik"
+    }
+  }, 
+}
+</script>
+
+<style scoped>
+  h1{
+    color: red;
+  }
+  div{
+    /* border: 2px solid brown; */
+    margin: 3rem;
+  }
+  .kk{
+    background-color: cornsilk;
+    padding: 3rem;
+  }
+</style>>
+
+</style>
---
+++ src/child.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Hello!! I am the child....</h1>
+    <h2>my name is {{childname}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props :["childname"]
+}
+</script>
+
+<style scoped>
+    h1{
+        color: blue;
+    }
+    div{
+        background-color: burlywood;
+    }
+</style>>
+
+</style>
